129ec5898a187748e33a661ae238064d70013117
SriramKannan2005 <166842369+SriramKannan2005@users.noreply.github.com>
2026-03-06 10:24:17 +0530

    added config.js

---
+++ assets/js/config.js
@@ -0,0 +1,14 @@
+// Firebase Configuration
+// This file contains your actual Firebase credentials
+// DO NOT COMMIT THIS FILE TO GIT!
+
+const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyAFq4BsqX69Gbo9TKD5CGWD4iMEwv4KPkQ",
+    authDomain: "chaitime-fa063.firebaseapp.com",
+    databaseURL: "https://chaitime-fa063-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "chaitime-fa063",
+    storageBucket: "chaitime-fa063.firebasestorage.app",
+    messagingSenderId: "357283195164",
+    appId: "1:357283195164:web:84ca7a59ba9b0d7f3132d9",
+    measurementId: "G-E0KP6BXQ6H"
+};
